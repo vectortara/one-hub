878f153a539bbf5d3eefb5b6bd72708969c9fb8a
Winter-Wine <yy346225@dal.ca>
2026-01-06 17:05:37 +0800

    change start date

--- web/src/views/Log/index.jsx
+++ web/src/views/Log/index.jsx
@@ -34,7 +34,7 @@ export default function Log() {
     username: '',
     token_name: '',
     model_name: '',
-    start_timestamp: 0,
+    start_timestamp: dayjs().unix(),
     end_timestamp: dayjs().unix() + 3600,
     log_type: '0',
     channel_id: '',

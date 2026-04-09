2f8ff1637aced2023a621a244dceda5970e51429
Winter-Wine <yy346225@dal.ca>
2026-04-09 16:06:39 +0800

    extend username validate

--- model/user.go
+++ model/user.go
@@ -19,7 +19,7 @@ import (
 // Otherwise, the sensitive information will be saved on local storage in plain text!
 type User struct {
 	Id               int            `json:"id"`
-	Username         string         `json:"username" gorm:"unique;index" validate:"max=12"`
+	Username         string         `json:"username" gorm:"unique;index" validate:"max=191"` //创建用户，更新用户会用到的验证字段
 	Password         string         `json:"password" gorm:"not null;" validate:"min=8,max=20"`
 	DisplayName      string         `json:"display_name" gorm:"index" validate:"max=20"`
 	Role             int            `json:"role" gorm:"type:int;default:1"`   // admin, common
